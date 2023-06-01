50c95bead68ca91a3d953c17c3abbd821b714664
Aziz Smaoui <ahmed.aziz.smaoui@gmail.com>
2023-06-01 20:18:38 +0100

    add categories | add items | homepage

--- biko/urls.py
+++ biko/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from core.views import index,contact
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
+    path('contact/',contact,name='contact'),
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
-]
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
