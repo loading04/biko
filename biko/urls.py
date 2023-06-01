580572d2baadb9782b164e03c9ad1fdead16e54f
Aziz Smaoui <ahmed.aziz.smaoui@gmail.com>
2023-06-01 20:45:51 +0100

    show details | show related items

--- biko/urls.py
+++ biko/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from core.views import index,contact
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
+    path('items/', include('item.urls')),
     path('contact/',contact,name='contact'),
     path('', index, name='index'),
     path('admin/', admin.site.urls),
