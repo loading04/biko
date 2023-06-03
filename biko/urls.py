d9926753787bf596ec9a8145afa721d0e84a1e2c
Aziz Smaoui <ahmed.aziz.smaoui@gmail.com>
2023-06-03 20:43:38 +0100

    dashboard page for users

--- biko/urls.py
+++ biko/urls.py
@@ -20,6 +20,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
+    path('dashboard/',include('dashboard.urls')),
     path('items/', include('item.urls')),
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
