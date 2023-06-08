b50353c800776a6a71387b9570197110634fa4c6
Aziz Smaoui <ahmed.aziz.smaoui@gmail.com>
2023-06-08 19:51:58 +0100

    browse items with filters

--- item/views.py
+++ item/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render,get_object_or_404,redirect
-from .models import Item
+from .models import Item , Category
 from .forms import NewItemForm , EditItemForm
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+
+def items(request):
+    
+    query = request.GET.get('query','')
+    category_id = request.GET.get('category',0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold = False )
+    
+    if category_id :
+        items = items.filter(category_id = category_id)
+    if query:
+        items = items.filter(Q(name__icontains = query) | Q(description__icontains = query) )
+    
+    return render(request, 'item/items.html',{
+        'items':items,
+        'query':query,
+        'categories':categories,
+        'category_id':int(category_id),
+    })
+
+
 # Create your views here.
 def detail (request , pk):
-    item = get_object_or_404(Item , pk=pk)
+    item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category=item.category,is_sold=False).exclude(pk=pk)[0:3]
     
     return render(request,'item/detail.html',{
