660e0f8b965bcf81b3b4b6c697907d661ce9be4e
Aziz Smaoui <ahmed.aziz.smaoui@gmail.com>
2023-06-11 15:04:19 +0100

    add items sold || profit

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from item.models import Item
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 # Create your views here.
 
 
@@ -8,7 +9,17 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def index(request):
     items = Item.objects.filter(created_by=request.user )
+    items_sold = Item.objects.filter(Q(created_by=request.user) & Q(is_sold=True) )
+    items_sold_count = items_sold.count()
     
+    
+    profit = 0
+    for item in items_sold:
+        profit += item.price
+
     return render(request,'dashboard/index.html',{
-        'items':items
+        'items':items,
+        'items_sold':items_sold_count,
+        'profit':profit,
     })
+
